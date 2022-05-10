54fed2f216194871997d80f6b2363a11187d939f
numpynewb33 <benlegrand@icloud.com>
2022-05-10 12:23:41 -0500

    adsi

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [
+      'metaopsgaming.com',
+      'www.prnewswire.com',
       'storage.googleapis.com',
       'avatars.dicebear.com',
       'avatars.githubusercontent.com',
