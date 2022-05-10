65d820b1d4e76557a271afbb2ac401028ee92f24
numpynewb33 <benlegrand@icloud.com>
2022-05-09 22:37:42 -0500

    logo

--- components/Header.js
+++ components/Header.js
@@ -36,7 +36,7 @@ const Header = ({ name, url }) => {
       <div className={style.headerLeft}>
         <Image
           className={style.facebookLogo}
-          src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png'
+          src='https://storage.googleapis.com/fractal-launchpad-public-assets/metaops-assets/images/10.jpg'
           alt=''
           height={30}
           width={30}
